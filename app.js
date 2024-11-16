1fa2b8137f24f6b2f61a8d2fd52c0050f48dcc04
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-11-16 18:12:15 +0500

    deployment

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const callRoutes = require("./routes/callRoutes");
 const industryRoutes = require("./routes/industryRoutes");
 
 const app = express();
-app.use(express.json());
+
+app.use(express.json({ limit: '50mb' }));
 app.use(
   cors({
     origin: "*",
