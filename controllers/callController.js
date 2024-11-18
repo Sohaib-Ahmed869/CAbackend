b032bb9dff6d2a3b168e9324796d6879fbd20a85
Sohaib-Ahmed869 <i211114@nu.edu.pk>
2024-11-18 15:07:46 +0500

    Twillio integrated

--- controllers/callController.js
+++ controllers/callController.js
@@ -4,14 +4,31 @@ const client = new twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_AUTH_TOKEN
 );
+const { db, auth } = require("../firebase");
 exports.initiateCall = async (req, res) => {
   try {
-    const customerNumber = "+15005550007"; // Test successful call
-    const supportNumber = "+15005550008"; // Test successful call
+    const UserId = req.params.user_id;
+
+    console.log("Initiating call for user:", UserId);
+
+    //get user phone number from database of firebase
+    const userCollection = db.collection("users");
+    const userDoc = await userCollection.doc(UserId).get();
+    const userData = userDoc.data();
+    const customerNumber = "+" + userData.phone;
+
+    console.log("User phone number:", customerNumber);
+
+    const supportNumber = "++61415171890"; // Test successful call
 
     // Start the call
     const call = await client.calls.create({
-      twiml: `<Response><Dial>${supportNumber}</Dial></Response>`,
+      twiml: `
+        <Response>
+          <Say>Hello, this is certified Australia. Our agent wants to connect with you. Please hold.</Say>
+          <Dial>${supportNumber}</Dial>
+        </Response>
+      `,
       to: customerNumber,
       from: process.env.TWILIO_PHONE_NUMBER,
     });
@@ -46,9 +63,7 @@ exports.initiateCall = async (req, res) => {
     pollCallStatus(call.sid);
 
     // Respond immediately, while the call is being tracked
-    res
-      .status(200)
-      .json({ message: "Call initiated and being tracked", callSid: call.sid });
+    res.status(200).json({ message: "Call initiated and being tracked" });
   } catch (error) {
     console.error("Error initiating call:", error);
     res.status(400).json({ message: "Failed to initiate call", error });
--- routes/callRoutes.js
+++ routes/callRoutes.js
@@ -2,5 +2,5 @@ const express = require("express");
 const router = express.Router();
 const callController = require("../controllers/callController");
 
-router.post("/initiate-call", callController.initiateCall);
+router.post("/initiate-call/:user_id", callController.initiateCall);
 module.exports = router;
