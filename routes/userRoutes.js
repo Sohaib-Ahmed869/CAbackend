a5751cc243e865f44c4d6d96997b1c16a538bc13
AdilRazzaq1919 <adilrazzaq1919@gmail.com>
2025-03-07 21:50:00 +0500

    Backend changes in document controller

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,25 +3,12 @@ const { authenticateUser } = require("../middleware/authMiddleware");
 const { allowRoles } = require("../middleware/roleMiddleware");
 const upload = require("../utils/multerconfig");
 const express = require("express");
-const {
-  registerUser,
-  verifyUser,
-  registerUserbyAgent,
-  changeEmail,
-  changePhoneNumber,
-} = require("../controllers/userController");
-const {
-  updateApplicationStatus,
-} = require("../controllers/applicationController");
-const {
-  DocumentsFormByApplicationId,
-} = require("../controllers/documentController");
-const {
-  StudentIntakeFormByApplicationId,
-} = require("../controllers/studentIntakeController");
-const {
-  updateInitialScreeningForm,
-} = require("../controllers/initialScreeningController");
+const multer = require("multer");
+const {registerUser,verifyUser,registerUserbyAgent,changeEmail,changePhoneNumber,} = require("../controllers/userController");
+const {updateApplicationStatus,} = require("../controllers/applicationController");
+const {DocumentsFormByApplicationId,  uploadSingleFile,deleteSingleFile,} = require("../controllers/documentController");
+const {StudentIntakeFormByApplicationId,} = require("../controllers/studentIntakeController");
+const {updateInitialScreeningForm,} = require("../controllers/initialScreeningController");
 
 const router = express.Router();
 
@@ -30,40 +17,10 @@ router.post("/registerByAgent", registerUserbyAgent);
 router.put("/phonenumber/:userId", changePhoneNumber);
 router.put("/email/:userId", changeEmail);
 router.put("/updateApplicationStatus/:applicationId", updateApplicationStatus);
-router.put(
-  "/documentUpload/:applicationId",
-  upload.fields([
-    { name: "idCard", maxCount: 1 },
-    { name: "australian_citizenship", maxCount: 1 },
-    { name: "license", maxCount: 1 },
-    { name: "passport", maxCount: 1 },
-    { name: "birth_certificate", maxCount: 1 },
-    { name: "medicare", maxCount: 1 },
-    { name: "creditcard", maxCount: 1 },
-    { name: "resume", maxCount: 1 },
-    { name: "previousQualifications", maxCount: 1 },
-    { name: "reference1", maxCount: 1 },
-    { name: "reference2", maxCount: 1 },
-    { name: "employmentLetter", maxCount: 1 },
-    { name: "payslip", maxCount: 1 },
-    { name: "image1", maxCount: 1 },
-    { name: "image2", maxCount: 1 },
-    { name: "image3", maxCount: 1 },
-    { name: "image4", maxCount: 1 },
-    { name: "video1", maxCount: 1 },
-    { name: "video2", maxCount: 1 },
-  ]),
-  DocumentsFormByApplicationId
-);
-
-router.put(
-  "/StudentIntakeFormByApplicationId/:applicationId",
-  upload.single("previousQualifications"),
-  StudentIntakeFormByApplicationId
-);
-router.put(
-  "/updateInitialScreeningForm/:applicationId",
-  updateInitialScreeningForm
-);
+router.post("/:applicationId/uploadSingle", upload.single("file"), uploadSingleFile);
+router.delete("/:applicationId/deleteSingle", deleteSingleFile);
+router.post("/:applicationId/submitDocument",DocumentsFormByApplicationId)
+router.put("/StudentIntakeFormByApplicationId/:applicationId",upload.single("previousQualifications"),StudentIntakeFormByApplicationId);
+router.put("/updateInitialScreeningForm/:applicationId",updateInitialScreeningForm);
 
 module.exports = router;
