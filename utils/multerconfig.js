91bab6c81172cd65d3cce8de427ebeac2784caf8
sohaib-ahmed869 <i211114@nu.edu.pk>
2025-03-17 02:41:22 +0500

    done

--- utils/multerconfig.js
+++ utils/multerconfig.js
@@ -6,18 +6,14 @@ const upload = multer({
   storage,
   fileFilter: (req, file, cb) => {
     if (
-      file.mimetype === "image/jpeg" ||
-      file.mimetype === "image/jpg" ||
-      file.mimetype === "image/png" ||
       file.mimetype === "application/pdf" ||
-      file.mimetype === "application/vnd.openxmlformats-officedocument.wordprocessingml.document" ||
-      file.mimetype === "video/mp4"||
-      file.mimetype === "video/quicktime"
+      file.mimetype === "image/png" ||
+      file.mimetype === "video/mp4"
     ) {
       cb(null, true);
     } else {
-      const error = new Error("Only PDF, PNG, JPG, DOCX, and MP4 files are allowed");
-      error.status = 404; 
+      const error = new Error("Only PDF and PNG files are allowed");
+      error.status = 400; // Optional: Add a custom status code
       cb(error, false);
     }
   },
