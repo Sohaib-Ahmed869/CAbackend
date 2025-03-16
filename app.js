7583bc543a9caad58169e4c477556fc4fd081797
sohaib-ahmed869 <i211114@nu.edu.pk>
2025-03-17 03:19:52 +0500

    ok

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin: "https://ca-ruby.vercel.app",
+    origin: "https://portal.certifiedaustralia.com.au",
   })
 );
 app.use(logRequest);
