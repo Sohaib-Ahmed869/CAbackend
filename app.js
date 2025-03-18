1cd76f4287f45ac6cd47a464a1c034a6be824fed
asadawan16 <asadawan16900@gmail.com>
2025-03-18 20:44:08 +0500

    ok

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
-    origin: "https://portal.certifiedaustralia.com.au",
+    // origin: "https://portal.certifiedaustralia.com.au",
+    origin: "http://localhost:5173",
   })
 );
 app.use(logRequest);
